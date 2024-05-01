b8197dd7c2029d701317515b0102eaef29451d1c
Jorge Gonzalez <jorge.gonzalez.piedra@gmail.com>
2024-05-01 18:26:34 +0200

    Initial commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import "./Navbar.css";
+import React, { useState } from 'react';
+
+const Navbar = () => {
+  // State to manage the navbar's visibility
+  const [nav, setNav] = useState(false);
+
+  // Toggle function to handle the navbar's display
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  // Array containing navigation items
+  const navItems = [
+    { id: 1, text: "Home" },
+    { id: 2, text: "Company" },
+    { id: 3, text: "Resources" },
+    { id: 4, text: "About" },
+    { id: 5, text: "Contact" },
+  ];
+
+  return (
+    <div className="bg-black flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      {/* Logo */}
+      {/* <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1> */}
+      {/* https://medium.com/@ryaddev/build-responsive-navbar-with-tailwind-css-and-react-icons-3b13a272dec4*/}
+
+      {/* Desktop Navigation */}
+      <ul className="hidden md:flex">
+        {navItems.map((item) => (
+          <li
+            key={item.id}
+            className="p-4 hover:bg-[#00df9a] rounded-xl m-2 cursor-pointer duration-300 hover:text-black"
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
