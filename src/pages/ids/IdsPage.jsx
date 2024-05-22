eba26bcf174f98031e884136ea0e29003cfed1c3
Jorge Gonzalez <jorge.gonzalez.piedra@gmail.com>
2024-05-22 22:30:15 +0200

    Added loading animation to document card Deleted loading animation from idsPage

--- src/pages/ids/IdsPage.jsx
+++ src/pages/ids/IdsPage.jsx
@@ -1,24 +1,7 @@
 import "./IdsPage.css";
 import DocumentCard from "../../components/cards/documentcard/DocumentCard.jsx";
-import { useState, useEffect } from "react";
 
 const IdsPage = () => {
-  const API_KEY = import.meta.env.VITE_API_KEY;
-
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      await fetch("https://personagen.fly.dev/actuator/health", {headers: {"api-key": API_KEY}});
-      setIsLoading(false);
-    };
-
-    fetchData();
-  }, []);
-
-  
-
   return (
         <div className="flex xl:flex-row sm:max-xl:items-center flex-col justify-center items-start mt-5 gap-5">
           <DocumentCard idType="NIF"></DocumentCard>
