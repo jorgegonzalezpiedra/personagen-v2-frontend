b7c939a3e2a5f4b9d14390e06847ac70a161d429
Jorge Gonzalez <jorge.gonzalez.piedra@gmail.com>
2024-05-24 23:00:19 +0200

    Updated styles

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -3,7 +3,7 @@ import ProfileCard from "../../components/cards/profilecard/ProfileCard.jsx";
 
 const ProfilePage = () => {
   return (
-    <div className="flex xl:flex-col sm:max-xl:items-center flex-col items-center items-start mt-5">
+    <div className="flex xl:flex-col sm:max-xl:items-center flex-col items-center mt-5">
       <ProfileCard profileType="user"></ProfileCard>
       <ProfileCard profileType="company"></ProfileCard>
     </div>
